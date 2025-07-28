e663f31ada19d5d12a5750e1ca00f19b7ed931a2
Daniel Roy Greenfeld <daniel@feldroy.com>
2025-07-28 15:08:51 +0800

    Support customizable rendering

--- src/air_markdown/__init__.py
+++ src/air_markdown/__init__.py
@@ -4,3 +4,4 @@ __author__ = """Daniel Roy Greenfeld"""
 __email__ = 'daniel@feldroy.com'
 
 from .tags import Markdown as Markdown
+from .tags import TailwindTypographyMarkdown as TailwindTypographyMarkdown
